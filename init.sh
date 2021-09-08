715dd4778d13132c9762d41745a7830b8bdf9734
Kostya2702 <loshkarev.kostya27@gmail.com>
2021-09-08 14:57:40 +0500

    change path hello

--- init.sh
+++ init.sh
@@ -1,4 +1,2 @@
-sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/wsgi_app
 sudo /etc/init.d/gunicorn restart
