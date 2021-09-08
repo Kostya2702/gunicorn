30bc66462106058909fee19037bb4b66fd49ecb5
Kostya2702 <loshkarev.kostya27@gmail.com>
2021-09-08 13:09:42 +0500

    Test gunicorn server

--- hello.py
+++ hello.py
@@ -2,12 +2,12 @@
 import re
 
 
-def wsgi_app(environ):
-    # start_responce('200', [('Content-type', 'text/plain')])
-    pattern = r'.*?(\?\w)'
-    body = re.findall(pattern, environ)
-    print(body)
+def wsgi_app(environ, start_responce):
+    start_responce('200 OK', [('Content-type', 'text/plain')])
+    pattern = r'(?:\w\=\d)+'
+    body = [bytes(f'{el}\n', 'ascii') for el in re.findall(pattern, environ)]
+    return body
 
 
-wsgi_app('/?a=1&a=2&b=3 ')
+# print(wsgi_app('/?a=1&a=2&b=3'))
 
